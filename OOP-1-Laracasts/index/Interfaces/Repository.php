1480de56da6fd393e972761f273eebbdab8276eb
leonardo cuquejo soares <leonardo.s@cuquejo.org>
2019-06-30 21:37:23 +0100

    OOP-1 ep8

---
+++ OOP-1-Laracasts/index/Interfaces/Repository.php
@@ -0,0 +1,18 @@
+<?php
+interface Repository{
+    public function save($data);
+}
+
+class MongoRepository implements Repository {
+    public function save($data)
+    {
+
+    }
+}
+
+class FileRepository implements Repository {
+    public function save($data)
+    {
+
+    }
+}
